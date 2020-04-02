6457eed685c2ddba15d922154b996946409ef64e
Joseph Montesano <jmmmontesano@gmail.com>
2020-04-02 14:51:27 -0400

    updated

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ console.log(process.env.API_TOKEN);
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  console.log("Server side api", apiToken);
   const authToken = req.get("Authorization");
-  console.log("User input api", req.get("Authorization"));
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
@@ -28,7 +27,6 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get("/movie", function handleGetMovies(req, res) {
   let response = moviedex;
-  console.log(response[1].genre);
 
   if (req.query.genre) {
     response = response.filter(movie =>
@@ -51,7 +49,17 @@ app.get("/movie", function handleGetMovies(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
