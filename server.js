2310312d8d083a62e30d8dc488b996fb46397bb1
Joseph Montesano <jmmmontesano@gmail.com>
2020-04-02 10:14:35 -0400

    it is done

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const moviedex = require("./moviedex.json");
+const cors = require("cors");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-  res.send("Hello, world!");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  console.log("Server side api", apiToken);
+  const authToken = req.get("Authorization");
+  console.log("User input api", req.get("Authorization"));
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
+
+app.get("/movie", function handleGetMovies(req, res) {
+  let response = moviedex;
+  console.log(response[1].genre);
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => movie.avg_vote > Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
 });
 
 const PORT = 8000;
